0c0926e54c9d5a57d9d02dec3223ef106eea2cdf
Kate <ket11141@gmail.com>
2018-03-28 13:57:03 +0300

    skip other chars

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-
+# Encryption and decryption for vigenere cipher
 
 def encrypt_symbol(letter, key, position, start):
     encrypt_letter = (ord(letter) - start + ord(key[position % len(key)]) - 65) % 26 + start
@@ -9,11 +9,14 @@ def encrypt_vigenera(info, key):
     encrypt_position = 0
     result = ''
     for i in range(len(info)):
-        if ord(info[i]) > 96:
-            result += encrypt_symbol(info[i], key, encrypt_position, 97)
+        if info[i].isalpha():
+            if ord(info[i]) > 96:
+                result += encrypt_symbol(info[i], key, encrypt_position, 97)
+            else:
+                result += encrypt_symbol(info[i], key, encrypt_position, 65)
+            encrypt_position += 1
         else:
-            result += encrypt_symbol(info[i], key, encrypt_position, 65)
-        encrypt_position += 1
+            result += info[i]
     return result
 
 
@@ -26,13 +29,18 @@ def decrypt_vigenera(info, key):
     decrypt_position = 0
     result = ''
     for i in range(len(info)):
-        if ord(info[i]) > 96:
-            result += decrypt_symbol(info[i], key, decrypt_position, 97)
+        if info[i].isalpha():
+            if ord(info[i]) > 96:
+                result += decrypt_symbol(info[i], key, decrypt_position, 97)
+            else:
+                result += decrypt_symbol(info[i], key, decrypt_position, 65)
+            decrypt_position += 1
         else:
-            result += decrypt_symbol(info[i], key, decrypt_position, 65)
-        decrypt_position += 1
+            result += info[i]
     return result
 
 
-print(encrypt_vigenera('ABC', 'ABC'))
-print(decrypt_vigenera('ACE', 'ABC'))
+key = 'abc'.upper()
+
+print(encrypt_vigenera('ABC DE?qwe', key))
+print(decrypt_vigenera('ACE DF?swf', key))
