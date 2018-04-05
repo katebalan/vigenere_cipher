302f1802c8dea287e55a0a7bb2857b663be63637
Kate <ket11141@gmail.com>
2018-04-05 12:20:44 +0300

    finished hack_length

--- lab1.py
+++ lab1.py
@@ -1,13 +1,14 @@
 # Encryption and decryption for vigenere cipher
 from vigenera_cipher import *
+from vigenera_hack import *
 
 # key = 'key'.upper()
 # info = 'silence is not empty, it is full of answers'
 
-key = raw_input("Please enter key: ").upper()
+key = input("Please enter key: ").upper()
 
 with open('normal_text.txt', 'r') as text_document:
-  info = text_document.read()
+    info = text_document.read()
 
 text_document.close()
 
@@ -19,7 +20,7 @@ text_document.close()
 
 
 with open('encrypted_text.txt', 'r') as text_document:
-  info = text_document.read()
+    info = text_document.read()
 
 text_document.close()
 
@@ -28,3 +29,5 @@ decrypt_msg = decrypt_vigenera(info, key)
 text_document = open('decrypted_text.txt', 'w')
 text_document.write(decrypt_msg)
 text_document.close()
+
+hack_key_length(encrypt_msg)
--- vigenera_hack.py
+++ vigenera_hack.py
@@ -4,8 +4,23 @@
 
 
 ALPH_SIZE = 26
+devide = "\n***********\n"
 
 
+# Return info as array of letters
+# without any other symbols
+def only_letters(info):
+    info = list(info)
+    normal_info = []
+    for i in range(len(info)):
+        if info[i].isalpha():
+            normal_info.append(info[i])
+    # print(info)
+    # print(normal_info)
+    return normal_info
+
+
+# Make information shift to one character to left
 def shift_left(shift):
     shift_list = shift
     shift_symbol = shift_list[0]
@@ -18,32 +33,35 @@ def shift_left(shift):
 
 
 def hack_key_length(encoded_info):
-    result = list(encoded_info)
-    shift = list(encoded_info)
+    result = only_letters(encoded_info)
+    shift = only_letters(encoded_info)
     compare_elements = {}
     length_key = []
 
     for j in range(ALPH_SIZE):
+        # print(devide)
+        # print(result)
         shift = shift_left(shift)
-        print("".join(shift))
+        # print("".join(shift))
+        # print(shift)
+        count = 0
 
         for i in range(len(result)):
             # print(result[i])
             # print(shift[i])
             if result[i] == shift[i]:
-                if compare_elements.get(j):
-                    compare_elements[j] += 1
-                else:
-                    compare_elements[j] = 1
+                count += 1
+                # print(result[i])
 
-        print(compare_elements)
-        if compare_elements[j] / len(result) > 0.06:
+        compare_elements[j] = count/len(result)
+
+        print(compare_elements[j])
+        if compare_elements[j] > 0.06:
             length_key.append(j + 1)
 
     print(length_key)
 
 
-encrypt_msg = "cmjorao mq xsr oqndc, gd mq pyjv sd krqgipc"
-devide = "\n***********\n"
-print(devide)
-hack_key_length(encrypt_msg)
+# encrypt_msg = "cmjorao mq xsr oqndc, gd mq pyjv sd krqgipc"
+# # print(devide)
+# hack_key_length(encrypt_msg)
